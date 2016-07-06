b821a9a09751d7513771c455dd7416ea02ffafd4
boxchilli marketing <boxchilli@boxchillis-iMac.home>
2016-07-06 15:48:07 +0100

    First commit

---
+++ parts/nav-offcanvas-topbar.php
@@ -0,0 +1,15 @@
+<div class="top-bar" id="top-bar-menu">
+	<div class="top-bar-left float-left">
+		<ul class="menu">
+			<li class="menu-text"><?php bloginfo('name'); ?></li>
+		</ul>
+	</div>
+	<div class="top-bar-right show-for-medium">
+		<?php joints_top_nav(); ?>
+	</div>
+	<div class="top-bar-right float-right show-for-small-only">
+		<ul class="menu">
+			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+		</ul>
+	</div>
+</div>
